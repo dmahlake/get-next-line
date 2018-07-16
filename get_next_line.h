6784bab6f9103ea53740168ea7575d415ab30d32
Dipuo MAHLAKE <dmahlake@e4r11p3.wethinkcode.co.za>
2018-07-16 15:14:59 +0200

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmahlake <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/22 11:28:27 by dmahlake          #+#    #+#             */
+/*   Updated: 2017/08/22 11:29:18 by dmahlake         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __GET_NEXT_LINE_H
+# define __GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <string.h>
+
+# include "libft/includes/libft.h"
+
+# define BUFF_SIZE 75
+
+int		get_next_line(const int fd, char **line);
+#endif
